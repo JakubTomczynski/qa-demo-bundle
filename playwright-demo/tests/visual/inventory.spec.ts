4b8f56b7853dc21c971ebb483cc55c038a932bf6
Jakub Tomczyński <jakub.tomczynski@softiq.pl>
2025-09-12 13:49:55 +0200

    test(visual): add inventory visual spec; chore(config): update package.json and playwright.config.ts

--- playwright-demo/tests/visual/inventory.spec.ts
+++ playwright-demo/tests/visual/inventory.spec.ts
@@ -1,7 +1,6 @@
 import { test } from '../../fixtures/classFixture';
 import { sauceLabsBoltShirt, sauceLabsBoltShirtDetails } from '../interfaces/orderSummaryInterface';
-test.use({ storageState: 'storageState.json' });
-test.describe.only('@visual base page', () => {
+test.describe('@visual base page', () => {
   test.beforeEach(async ({ page, loginPage }) => {
     await page.goto('/');
     await page.waitForLoadState('networkidle');
